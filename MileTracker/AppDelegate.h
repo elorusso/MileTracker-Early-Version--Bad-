c395d6f0862529424f6e628eb37c67c5e27f3f74
Emanuel <emanuel@Macintosh-6.local>
2012-09-17 15:06:47 -0400

    Initial Commit

---
+++ MileTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MileTracker
+//
+//  Created by Emanuel on 9/17/12.
+//  Copyright (c) 2012 Emanuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ MileTracker/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MileTracker
+//
+//  Created by Emanuel on 9/17/12.
+//  Copyright (c) 2012 Emanuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
